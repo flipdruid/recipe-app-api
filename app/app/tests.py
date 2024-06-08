02f55b3ad4b68db134c049f06c47df01f51e194a
flipdruid <flipdruid@yahoo.com>
2024-06-09 04:55:45 +0800

    08 - 003.4

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,9 +3,9 @@ from . import calc
 
 class CalcTest(SimpleTestCase):
     def test_add_numbers(self):
-        res = calc.add(5,6)
-        self.assertEqual(res,11)
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        res = calc.subtract(7,5)
-        self.assertEqual(res,2)
+        res = calc.subtract(7, 5)
+        self.assertEqual(res, 2)
