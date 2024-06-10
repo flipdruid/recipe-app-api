19e34a330579c49f0d5be0230eff5e998818ea1f
flipdruid <flipdruid@yahoo.com>
2024-06-11 01:35:50 +0800

    11.06.2024

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,8 +1,9 @@
 from django.test import SimpleTestCase
 from . import calc
 
+
 class CalcTest(SimpleTestCase):
-    
+
     def test_add_numbers(self):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
